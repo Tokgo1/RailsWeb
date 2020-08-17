2957dc489566b4f10aa135f8f874cd3443cff59f
PongLyon <ponglyon@qq.com>
2020-08-17 15:44:28 +0800

    sample app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 
   resources :microposts
   resources :users
@@ -9,5 +10,7 @@ Rails.application.routes.draw do
   # 有关此文件中可用DSL的详细信息，请参见http://guides.rubyonrails.org/routing.html
 
   # root 'application#hello'
-  root 'users#index'
+  # root 'users#index'
+  root 'static_pages#contact'
+
 end
